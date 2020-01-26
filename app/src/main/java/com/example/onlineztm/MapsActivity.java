f6aa60178f9c23c1b673e193fb964ea98631609e
jsarnowski96 <j.sarnowski@linux.pl>
2020-01-26 19:51:13 +0100

    Implementation of JSON parser

--- app/src/main/java/com/example/onlineztm/MapsActivity.java
+++ app/src/main/java/com/example/onlineztm/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.onlineztm;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,9 +14,27 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    ProgressDialog pd;
+    JSONObject busStopsJson;
+    JSONObject busStop;
+    JsonTask jsonTask;
+    ArrayList<LatLng> busStops;
+
+    String busStopName;
+    String busStopId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        busStops = new ArrayList();
+        jsonTask = new JsonTask();
     }
 
 
@@ -40,13 +64,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tricity = new LatLng(54.410240,18.580600);
+        mMap.addMarker(new MarkerOptions().position(tricity).title("Tricity Center mass"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tricity));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(tricity, 15.0f));
     }
 
     @Override
     public void onBackPressed() {
         finish();
+    }
+
+    private class JsonTask extends AsyncTask<String, String, JSONObject> {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(MapsActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+                busStopsJson = new JSONObject(buffer.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject response)
+        {
+            if(response != null)
+            {
+                try {
+                    Log.e("App", "Success: " + response.getString("JsonElement") );
+                } catch (JSONException ex) {
+                    Log.e("App", "Failure", ex);
+                }
+            }
+        }
     }
 }
