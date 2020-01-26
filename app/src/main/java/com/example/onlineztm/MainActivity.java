9fc0d696c6f24750ac9cb07d4ba75be3a146bd92
jsarnowski96 <j.sarnowski@linux.pl>
2020-01-26 21:07:12 +0100

    JSON Parser cd.

--- app/src/main/java/com/example/onlineztm/MainActivity.java
+++ app/src/main/java/com/example/onlineztm/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.onlineztm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.common.api.GoogleApiActivity;
-import com.google.android.gms.maps.GoogleMap;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/onlineztm/MapsActivity.java
+++ app/src/main/java/com/example/onlineztm/MapsActivity.java
@@ -1,11 +1,9 @@
 package com.example.onlineztm;
 
-import androidx.fragment.app.FragmentActivity;
-
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,23 +16,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    ProgressDialog pd;
     JSONObject busStopsJson;
-    JSONObject busStop;
     JsonTask jsonTask;
-    ArrayList<LatLng> busStops;
-
-    String busStopName;
-    String busStopId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +37,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        busStops = new ArrayList();
         jsonTask = new JsonTask();
     }
 
@@ -64,7 +55,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng tricity = new LatLng(54.410240,18.580600);
+        LatLng tricity = new LatLng(54.410240, 18.580600);
         mMap.addMarker(new MarkerOptions().position(tricity).title("Tricity Center mass"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(tricity));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(tricity, 15.0f));
@@ -75,50 +66,215 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         finish();
     }
 
-    private class JsonTask extends AsyncTask<String, String, JSONObject> {
+    public class BusStops {
+        private String lastUpdate;
+        private Stop[] stops;
+
+        public String getLastUpdate() { return lastUpdate; }
+        public void setLastUpdate(String value) { this.lastUpdate = value; }
+
+        public Stop[] getStops() { return stops; }
+        public void setStops(Stop[] value) { this.stops = value; }
+    }
+
+    public class Stop {
+        private Long stopID;
+        private String stopCode;
+        private String stopName;
+        private String stopShortName;
+        private String stopDesc;
+        private String subName;
+        private String date;
+        private Long zoneID;
+        private ZoneName zoneName;
+        private Long virtual;
+        private Long nonpassenger;
+        private Long depot;
+        private Long ticketZoneBorder;
+        private Long onDemand;
+        private String activationDate;
+        private Double stopLat;
+        private Double stopLon;
+        private String stopURL;
+        private Object locationType;
+        private Object parentStation;
+        private String stopTimezone;
+        private Object wheelchairBoarding;
+
+        public Long getStopID() { return stopID; }
+        public void setStopID(Long value) { this.stopID = value; }
+
+        public String getStopCode() { return stopCode; }
+        public void setStopCode(String value) { this.stopCode = value; }
+
+        public String getStopName() { return stopName; }
+        public void setStopName(String value) { this.stopName = value; }
+
+        public String getStopShortName() { return stopShortName; }
+        public void setStopShortName(String value) { this.stopShortName = value; }
+
+        public String getStopDesc() { return stopDesc; }
+        public void setStopDesc(String value) { this.stopDesc = value; }
+
+        public String getSubName() { return subName; }
+        public void setSubName(String value) { this.subName = value; }
+
+        public String getDate() { return date; }
+        public void setDate(String value) { this.date = value; }
+
+        public Long getZoneID() { return zoneID; }
+        public void setZoneID(Long value) { this.zoneID = value; }
+
+        public ZoneName getZoneName() { return zoneName; }
+        public void setZoneName(ZoneName value) { this.zoneName = value; }
+
+        public Long getVirtual() { return virtual; }
+        public void setVirtual(Long value) { this.virtual = value; }
+
+        public Long getNonpassenger() { return nonpassenger; }
+        public void setNonpassenger(Long value) { this.nonpassenger = value; }
+
+        public Long getDepot() { return depot; }
+        public void setDepot(Long value) { this.depot = value; }
+
+        public Long getTicketZoneBorder() { return ticketZoneBorder; }
+        public void setTicketZoneBorder(Long value) { this.ticketZoneBorder = value; }
+
+        public Long getOnDemand() { return onDemand; }
+        public void setOnDemand(Long value) { this.onDemand = value; }
+
+        public String getActivationDate() { return activationDate; }
+        public void setActivationDate(String value) { this.activationDate = value; }
+
+        public Double getStopLat() { return stopLat; }
+        public void setStopLat(Double value) { this.stopLat = value; }
+
+        public Double getStopLon() { return stopLon; }
+        public void setStopLon(Double value) { this.stopLon = value; }
+
+        public String getStopURL() { return stopURL; }
+        public void setStopURL(String value) { this.stopURL = value; }
+
+        public Object getLocationType() { return locationType; }
+        public void setLocationType(Object value) { this.locationType = value; }
+
+        public Object getParentStation() { return parentStation; }
+        public void setParentStation(Object value) { this.parentStation = value; }
+
+        public String getStopTimezone() { return stopTimezone; }
+        public void setStopTimezone(String value) { this.stopTimezone = value; }
+
+        public Object getWheelchairBoarding() { return wheelchairBoarding; }
+        public void setWheelchairBoarding(Object value) { this.wheelchairBoarding = value; }
+    }
+
+    public enum ZoneName {
+        GDASK, GDYNIA, KOLBUDY, PRUSZCZ_GD_GMINA, PRUSZCZ_GD_MIASTO, SOPOT, UKOWO;
+
+        public String toValue() {
+            switch (this) {
+                case GDASK: return "Gda\u0144sk";
+                case GDYNIA: return "Gdynia";
+                case KOLBUDY: return "Kolbudy";
+                case PRUSZCZ_GD_GMINA: return "Pruszcz Gd. - gmina";
+                case PRUSZCZ_GD_MIASTO: return "Pruszcz Gd. - miasto";
+                case SOPOT: return "Sopot";
+                case UKOWO: return "\u017bukowo";
+            }
+            return null;
+        }
+
+        public static ZoneName forValue(String value) throws IOException {
+            if (value.equals("Gda\u0144sk")) return GDASK;
+            if (value.equals("Gdynia")) return GDYNIA;
+            if (value.equals("Kolbudy")) return KOLBUDY;
+            if (value.equals("Pruszcz Gd. - gmina")) return PRUSZCZ_GD_GMINA;
+            if (value.equals("Pruszcz Gd. - miasto")) return PRUSZCZ_GD_MIASTO;
+            if (value.equals("Sopot")) return SOPOT;
+            if (value.equals("\u017bukowo")) return UKOWO;
+            throw new IOException("Cannot deserialize ZoneName");
+        }
+    }
+
+    private class JsonTask extends AsyncTask<String, Void, JSONObject> {
+        @Override
         protected void onPreExecute() {
-            super.onPreExecute();
-            pd = new ProgressDialog(MapsActivity.this);
-            pd.setMessage("Please wait");
-            pd.setCancelable(false);
-            pd.show();
+            // Do stuff before the operation
         }
 
         @Override
-        protected JSONObject doInBackground(String... params) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
+        protected JSONObject doInBackground(String... params){
+            HttpURLConnection urlConnection = null;
+            String urlString = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json";
+            URL url = null;
+            try {
+                url = new URL(urlString);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                urlConnection.setRequestMethod("GET");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            urlConnection.setReadTimeout(10000 /* milliseconds */ );
+            urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+            try {
+                urlConnection.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new InputStreamReader(url.openStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            while (true) {
+                try {
+                    if (!((line = br.readLine()) != null)) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                sb.append(line);
+            }
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String jsonString = sb.toString();
+            System.out.println("JSON: " + jsonString);
 
             try {
-                URL url = new URL("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json");
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
+                return new JSONObject(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
-                while((line = reader.readLine()) != null){
-                    buffer.append(line);
-                }
-                busStopsJson = new JSONObject(buffer.toString());
-            } catch (Exception e) {
+            try {
+                return new JSONObject(br.toString());
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
             return null;
         }
 
         @Override
-        protected void onPostExecute(JSONObject response)
-        {
-            if(response != null)
-            {
-                try {
-                    Log.e("App", "Success: " + response.getString("JsonElement") );
-                } catch (JSONException ex) {
-                    Log.e("App", "Failure", ex);
-                }
-            }
+        protected void onPostExecute(JSONObject result) {
+            // Do stuff after the operation
         }
     }
+
 }
+
